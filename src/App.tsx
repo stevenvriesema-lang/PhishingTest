8f9db87a113fed51e57779cb2c1f0ab6f4061980
User <user@example.com>
2026-02-26 19:02:44 +0100

    Make white bar narrower

--- src/App.tsx
+++ src/App.tsx
@@ -55,15 +55,15 @@ function App() {
       {/* Header with centered logo */}
       <header className="bg-[#09529a] py-2">
         <div className="flex justify-center">
-          <div className="w-1/4"></div>
-          <div className="w-1/2 bg-white rounded-lg p-2 flex justify-center">
+          <div className="w-1/3"></div>
+          <div className="w-1/3 bg-white rounded-lg p-2 flex justify-center max-w-xs">
             <img 
               src="https://terwille.nl/wp-content/uploads/elementor/thumbs/Logo-Terwille-quv3ffjb1m15we0fi4hoyd4okjrgf9ca20xl99s2hs.jpg" 
               alt="Terwille" 
               className="h-12 w-auto"
             />
           </div>
-          <div className="w-1/4"></div>
+          <div className="w-1/3"></div>
         </div>
       </header>
 
