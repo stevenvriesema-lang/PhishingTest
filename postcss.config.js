69a6782019387c61abb789babc5a6ef958bd92e4
User <user@example.com>
2026-02-26 16:27:37 +0100

    Fix: Resolve postcss.config.js conflicts

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
 export default {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
 }
-=======
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
->>>>>>> 67edcca6ce146a05e10bf170df2762054b803a59
